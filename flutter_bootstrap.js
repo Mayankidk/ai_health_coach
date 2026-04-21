e44d6d69b6f43eccdcde9fb5702da02d5466add0
Mayankidk <Mayankidk@users.noreply.github.com>
2026-04-21 17:51:05 +0000

    deploy: 44ec940238a24cb9fa45097eda90e0927de2d871

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2248606061" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1656674107" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
